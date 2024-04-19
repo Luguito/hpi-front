85a8e6e17a316e99b501fa50a7e946feec37997f
Fuego <fgomez@dexfreight.io>
2024-04-18 20:00:08 -0500

    fix weight timeline

--- src/app/shared-services/ui/timeline.tsx
+++ src/app/shared-services/ui/timeline.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 // Icons
 import Icon1 from '../../../../public/shared-services/timeline/tap.svg'
 import Icon2 from '../../../../public/shared-services/timeline/productivity.svg'
-import Icon3 from '../../../../public/shared-services/timeline/lightbulb-setting.svg'
+import Icon3 from '../../../../public/shared-services/timeline/normalbulb-setting.svg'
 import Icon4 from '../../../../public/shared-services/timeline/expand-arrows.svg'
 import Icon5 from '../../../../public/shared-services/timeline/arrow-progress.svg'
 
@@ -34,7 +34,7 @@ export const TimelineComponent = () => {
                             <motion.p className="text-[16px] text-[#009BDE] font-medium" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>Ideation</motion.p>
                             <motion.h3 className="text-[50px] text-[#002E6D] font-extrabold" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>2012</motion.h3>
                         </article>
-                        <motion.p className="text-[16px] text-[#494949] font-light" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
+                        <motion.p className="text-[16px] text-[#494949] font-normal" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
                             Hutchison Ports forms a task force of operations professionals and technology experts to rethink <br />
                             terminal operations. Within 3 months, a groundbreaking operational model is conceived with an <br />
                             aggressive rollout plan.
@@ -54,7 +54,7 @@ export const TimelineComponent = () => {
                             <motion.p className="text-[16px] text-[#009BDE] font-medium" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>Embarkment</motion.p>
                             <motion.h3 className="text-[50px] text-[#002E6D] font-extrabold" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>2013</motion.h3>
                         </article>
-                        <motion.p className="text-[16px] text-[#494949] font-light" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
+                        <motion.p className="text-[16px] text-[#494949] font-normal" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
                             The first ROC is established in Asia, marking the beginning of a new era in port operations management.
                         </motion.p>
                     </section>
@@ -72,7 +72,7 @@ export const TimelineComponent = () => {
                             <motion.p className="text-[16px] text-[#009BDE] font-medium" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>Growth</motion.p>
                             <motion.h3 className="text-[50px] text-[#002E6D] font-extrabold" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>2014</motion.h3>
                         </article>
-                        <motion.p className="text-[16px] text-[#494949] font-light" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
+                        <motion.p className="text-[16px] text-[#494949] font-normal" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
                             ROC Centre begins providing support for all planning activities at terminals in Pakistan (SAPT/KICT), resulting in a 30% increase in productivity.
                             ROC expands services to the Middle East and Africa (MEA) region within a year.
                         </motion.p>
@@ -91,7 +91,7 @@ export const TimelineComponent = () => {
                             <motion.p className="text-[16px] text-[#009BDE] font-medium" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>Expansion</motion.p>
                             <motion.h3 className="text-[50px] text-[#002E6D] font-extrabold" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>2020</motion.h3>
                         </article>
-                        <motion.p className="text-[16px] text-[#494949] font-light" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
+                        <motion.p className="text-[16px] text-[#494949] font-normal" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
                             ROC{"'"}s success leads to its implementation at HIT, Hutchison Ports{"'"} flagship terminal in Hong Kong.
                             Managing thousands of vessels monthly, the ROC implementation led to a 60% reduction in human error.
                         </motion.p>
@@ -110,7 +110,7 @@ export const TimelineComponent = () => {
                             <motion.p className="text-[16px] text-[#009BDE] font-medium" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}> Future Directions</motion.p>
                             <motion.h3 className="text-[50px] text-[#002E6D] font-extrabold" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>2024</motion.h3>
                         </article>
-                        <motion.p className="text-[16px] text-[#494949] font-light" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
+                        <motion.p className="text-[16px] text-[#494949] font-normal" initial="hidden" whileInView="visible" variants={RevealTextAfterSection}>
                             Hutchison Ports will introduce NOC in Egypt, set to manage all its operational planning and back-office operations, showcasing our commitment to pushing the boundaries.
                         </motion.p>
                     </section>
