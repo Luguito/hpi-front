fe67950433122d906820de403e92a67eae8f6f13
Fuego <fgomez@dexfreight.io>
2024-06-19 15:28:32 -0500

    fix: get token

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from 'next/headers'
 
 export async function POST(req: NextRequest) {
+    const cookieStore = cookies();
+
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_STORAGE_URL}/api/authorize/login`, {
             method: "POST",
@@ -16,16 +19,8 @@ export async function POST(req: NextRequest) {
 
         const data = await res.json();
 
-        // Guardar el token en localStorage
-        if (typeof window !== 'undefined') {
-            localStorage.setItem('authToken', data.token);
-        }
 
-        let nextRes = NextResponse.json({ token: data.token })
-
-        nextRes.cookies.set('auth', data.token, { httpOnly: true, secure: true, path: '/' });
-
-        return nextRes;
+        return NextResponse.json({ token: data.token });
     } catch (e) {
         console.log(e)
     }
